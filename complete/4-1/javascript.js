aff8130c0f25031e58566d2d2e3afca1321d9c2b
hondaminori <hondaminori.ta3@gmail.com>
2021-04-24 13:42:29 +0900

    first commit

---
+++ complete/4-1/javascript.js
@@ -0,0 +1,26 @@
+// 課題4-1-1: 変数に文字を代入して、表示する。
+let str = '変数に文字を代入して、表示する。';
+console.log(str);
+
+// 課題4-1-2: 演算結果を表示する。
+// let result = 1 + 1;
+// console.log(result);
+
+// 課題4-1-3: 演算結果を表示する。
+// let result;
+// console.log(result);
+
+// 課題4-1-4: 演算結果を表示する。
+// console.log(result);
+
+// 課題4-1-5: 2つの変数a, bに値を入れて、足し算して表示する。
+// let a = 3;
+// let b = 4;
+
+// console.log(a + b);
+
+// 課題4-1-6: 「a == b (イコール2つです)」を表示させると・・・？
+// console.log(a == b);
+
+// 課題4-1-7: 「a = a + 1 」を表示させると・・・？
+// console.log(a = a + 1);
